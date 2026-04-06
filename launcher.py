ff67368fdfc259a08206216fb3ebdd01b69374a9
theredenemy <1234corbin.1234ad@gmail.com>
2026-04-06 01:18:30 -0700

    modified:   launcher.py

--- launcher.py
+++ launcher.py
@@ -2,6 +2,7 @@ import requests
 import configHelper
 import json
 import os
+import win32_functions
 config_file = "receiver.ini"
 
 ord_server_ip = configHelper.read_config(config_file, "ORD_SERVER", "ip", default_value="10.0.0.100")
@@ -16,8 +17,12 @@ mode = json_data.get('mode')
 print(mode)
 
 if mode == "game":
+    if win32_functions.get_pid_window("gris_paint.exe"):
+        os.system("taskkill /f /im gris_paint.exe")
     os.system("python main.py")
 elif mode == "draw":
+    if win32_functions.get_pid_window("DELTARUNE.exe"):
+        os.system("taskkill /f /im DELTARUNE.exe")
     os.system("python gris_paint_ord.py")
 
 
