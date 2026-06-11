7522ac8af1d9f04a4c1f415a363a930fca2b89c8
theredenemy <1234corbin.1234ad@gmail.com>
2026-06-10 19:08:56 -0700

    modified:   UploadFiles.py

--- UploadFiles.py
+++ UploadFiles.py
@@ -1,6 +1,6 @@
 import paramiko
 import os
-
+import fileinuse_functions
 def upload_dir(dir, remotepath, host, port, user, ssh_keyfile):
     print(f"Uploading {dir}")
     ssh_client = paramiko.SSHClient()
@@ -17,6 +17,8 @@ def upload_dir(dir, remotepath, host, port, user, ssh_keyfile):
         filepath = os.path.join(dir, file)
         basefile = os.path.basename(filepath)
         if os.path.isfile(filepath):
+            while fileinuse_functions.is_file_in_use(filepath):
+                pass
             print(filepath)
             sftp.put(localpath=filepath, remotepath=f"/{remotepath}/{basefile}")
         else:
@@ -41,6 +43,8 @@ def upload_file(filepath, remotepath, host, port, user, ssh_keyfile):
     basefile = os.path.basename(filepath)
     if os.path.isfile(filepath):
         print(filepath)
+        while fileinuse_functions.is_file_in_use(filepath):
+            pass
         sftp.put(localpath=filepath, remotepath=f"/{remotepath}/{basefile}")
         print(f"Done Uploading {filepath}")
         return True
